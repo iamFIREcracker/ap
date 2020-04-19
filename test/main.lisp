7cdebad731feedc78782b97d2f13b3cbc8d23f9c
Matteo Landi <matteo@matteolandi.net>
2020-04-19 11:50:17 +0200

    Add a few simple tests

--- test/main.lisp
+++ test/main.lisp
@@ -3,5 +3,12 @@
 
 (in-package :ap/tests)
 
+(1am:test ap/simple/completion-day
+  (1am:is (= 15 (completion-day (uiop:read-file-string #P"test/simple.txt")))))
+
+(1am:test ap/simple/completion-day/preallocations-ignored
+  (1am:is (= 10 (completion-day (uiop:read-file-string #P"test/simple.txt")
+                                :ignore-preallocations T))))
+
 (1am:test ap/known-scenario/completion-day
   (1am:is (= 16.25 (completion-day (uiop:read-file-string #P"test/known-scenario.txt")))))
